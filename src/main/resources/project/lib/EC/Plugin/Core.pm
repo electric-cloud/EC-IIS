2fd0c7ebb67a12977e5df56dcf00d0c3517279e1
Polina Shubina <925043@gmail.com>
2017-08-09 14:21:54 +0300

    Leaving logs as is

--- src/main/resources/project/lib/EC/Plugin/Core.pm
+++ src/main/resources/project/lib/EC/Plugin/Core.pm
@@ -268,7 +268,7 @@ sub bail_out {
     $msg .= "\n";
 
     $self->error();
-    $self->out(0, "BAILED_OUT:\n$msg\n");
+    $self->out(1, "BAILED_OUT:\n$msg\n");
     $self->set_property(summary => $msg);
     exit 1;
 }
@@ -485,7 +485,7 @@ sub run_command {
 
     my $cmd_to_display = join '', @cmd;
     $cmd_to_display = $self->safe_cmd($cmd_to_display);
-    $self->out(0, "Running command: " . $cmd_to_display);
+    $self->out(1, "Running command: " . $cmd_to_display);
     if ($self->dryrun()) {
         $self->dbg("Running command in dryrun mode");
         return {
@@ -593,9 +593,7 @@ sub _syscall_win32 {
         $ENV{NOPAUSE} = 1;
     }
 
-    no warnings 'io';
     my $pid = system($command);
-
     my $retval = {
         stdout => '',
         stderr => '',
